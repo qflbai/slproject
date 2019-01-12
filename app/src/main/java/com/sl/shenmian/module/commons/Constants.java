35e231d7c92097abb3567ea05034d15273498e2c
eric_caof <eric_caof@163.com>
2019-01-12 16:47:19 +0800

    添加站点和车牌信息

--- app/src/main/java/com/sl/shenmian/module/commons/Constants.java
+++ app/src/main/java/com/sl/shenmian/module/commons/Constants.java
@@ -2,7 +2,12 @@ package com.sl.shenmian.module.commons;
 
 import android.os.Environment;
 
+import com.sl.shenmian.module.main.pojo.CarLic;
+import com.sl.shenmian.module.main.pojo.Station;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Constants {
 
@@ -11,5 +16,33 @@ public class Constants {
         public final static String SDPATH = Environment.getExternalStorageDirectory().getPath()
                 + File.separator+"SL"+ File.separator;
         public final static String IMAGE_PATH = SDPATH + "images"+File.separator;
+    }
+
+    /**
+     * 站点信息
+     */
+    public static class LocalStation{
+        /**
+         * 关口地址
+         */
+        public static List<Station>  customs = new ArrayList<>();
+        /**
+         * 仓库地址
+         */
+        public static List<Station>  wareHouses = new ArrayList<>();
+        /**
+         * 门店地址
+         */
+        public static List<Station>  stores = new ArrayList<>();
+    }
+
+    /**
+     * 车牌信息
+     */
+    public static class LocalCarLic{
+        /**
+         * 关口地址
+         */
+        public static List<CarLic>  carLics = new ArrayList<>();
     }
 }
